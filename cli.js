d2d9ccb599081b03fccfcfeb84723aeb2264efec
roxsyVq <roxsy.dj@gmail.com>
2023-03-21 12:22:31 -0500

    optiones true las dos

--- cli.js
+++ cli.js
@@ -16,17 +16,33 @@ const opcion2 = process.argv[4]
 
 if (path) {
     if (opcion1 === undefined && opcion2 === undefined) {
+        console.log("llamada sin opciones")
+
         mdLinks(path, { validate: false, stats: false })
-            .then(result => result)
+
+            .then(result =>{
+                result.forEach(element => {
+                    console.log(`${element.href}  ${element.text}`)
+                    
+                });
+            })
+
+
     } else if (opcion1 === '--validate' && opcion2 === undefined) {
+        console.log("llamada opcion validate")
         mdLinks(path, { validate: true, stats: false })
-            .then(result => result)
+            .then(result => console.log("--validate",result))
     } else if (opcion1 === '--stats' && opcion2 === undefined) {
         mdLinks(path, { validate: false, stats: true })
             .then(result => result)
     } else if ((opcion1 === '--validate' && opcion2 === '--stats') || (opcion1 === '--stats' && opcion2 === '--validate')) {
         mdLinks(path, { validate: true, stats: true })
-            .then(result => result)
+            .then((result) =>  {
+                console.log(result )
+                //console.log(`Total: ${totalLinks}`);
+                //console.log(`Broken: ${brokenLinks.length}`);
+                
+            })
     } else {
         console.log('error en parametro')
     }
--- index.js
+++ index.js
@@ -12,68 +12,73 @@ const {
 } = require("./app");
 // mdLinks(path, options)
 
-const mdLinks = (path, options = {validate: false, stats: false }) => {
+const mdLinks = (path, options = { validate: false, stats: false }) => {
+  return new Promise((resolve, reject) => {
+    if (fileExists(path)) {
+      if (!checkPath(path)) {
+        getAbsolutePath(path).then((absolutePath) => {
+          const files = checkDir(absolutePath);
+          const renderFile = files.map((file) => extractLinks(file));
 
-    return new Promise((resolve, reject) => {
-      
-     console.log("lnks")
-           if (fileExists(path)) {
-              if (!checkPath(path)) {
-                getAbsolutePath(path)
-                  .then((absolutePath) => {
-                    const files = checkDir(absolutePath);
-                    const renderFile = files.map((file) => extractLinks(file));
-                    
-                    const promesa = Promise.all(renderFile);
-                    promesa.then((result) => {
-                      //console.log("resul",result.href)
-                      const planarArray = result.reduce(
-                        (acc, val) => acc.concat(val),
-                        []
-                      );
-                     planarArray.forEach(element => {
-                      console.log("Links",element.href)
-                     });
-                   /*  processLinks(planarArray)
+          const promesa = Promise.all(renderFile);
+          promesa.then((result) => {
+            //console.log("resul",result.href)
+            const planarArray = result.reduce(
+              (acc, val) => acc.concat(val),
+              []
+            );
+            planarArray.forEach((element) => {
+              // console.log("Links",element)
+            });
+            if (options.validate === false && options.stats === false) {
+              resolve(planarArray);
+            }
+            // resolve(planarArray)
+
+            /*  processLinks(planarArray)
                      .then((result) => {
                       resolve("mensaje", result)
                       }) 
                      .catch((error)=> console.log("error",error));*/
-                     // console.log("validate",  validate)
-                      
-                    
-            //reject  
+            // console.log("validate",  validate)
+
+            //reject
             if (options.validate === true && options.stats === true) {
-              console.log("dos opciones") 
-              const statc = statsValidate(planarArray)
-              console.log("stact",statc)
+              processLinks(planarArray).then((links) => {
+                // Contar el total de enlaces
+                const totalLinks = links.length;
+                //console.log(`Total: ${totalLinks}`);
 
+                // Contar enlaces rotos
+                const brokenLinks = links.filter((link) => link.status !== 200);
+                //console.log(`Broken: ${brokenLinks.length}`);
 
-            }
-            else if (options.validate === false && options.stats === true) {
-              console.log(" opcion stats")
-            }
-            else if (options.validate === true && options.stats === false) {
-              console.log("opcion validate ")
-            }
+                // Contar enlaces únicos
+                const uniqueLinks = [
+                  ...new Set(links.map((link) => link.href)),
+                ];
+               // console.log(`Unique : ${uniqueLinks.length}`);
 
+                const results = {
+                  total: totalLinks,
+                  broken: brokenLinks.length,
+                  unique: uniqueLinks.length
+                }
+                resolve( results)
+              });
+            } else if (options.validate === false && options.stats === true) {
+              console.log(" opcion stats");
+            } else if (options.validate === true && options.stats === false) {
+              console.log("opcion validate ");
+            }
           });
-        })
+        });
+      }
+    } else {
+      console.log("Please error");
+      //console.log(path)
     }
+  });
+};
 
-    
-  } 
-
-
-             else {
-                console.log('Please error');
-                //console.log(path)
-            }
-            
-        
-    })
-
-}
-
-module.exports = { mdLinks }
-
+module.exports = { mdLinks };
