7139c112151c2d711a080b423a548742b9a6bff8
roxsyVq <roxsy.dj@gmail.com>
2023-03-21 05:17:32 -0500

    optiones

--- cli.js
+++ cli.js
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
 //console.log('hello wordl')
 
+const { error } = require('console');
 const process = require('process');
-//const { mdLinks } = require('./index.js');
+const { mdLinks } = require('./index.js');
 //console.log(process)
 //argv  argumentos que vamos a escribir en la terminal
 
 const route = process.argv[2]
-console.log(route)
+//console.log(route)
 const opcion1 = process.argv[3]
-console.log(opcion1)
+//console.log(opcion1) validate
 const opcion2 = process.argv[4]
-console.log(opcion2)
+//console.log(opcion2) stats
+
+if (route) {
+    if (opcion1 === undefined && opcion2 === undefined) {
+      const promesas = mdLinks(route, { validate: false, stats: false });
+      console.log("promesas", promesas)
+
+            promesas.then(result => console.log("result",result)) .catch((error) => console.log("error",error))
+
+           // console.log("mostrar solo solo ruta")
+    }else {
+        console.log('error')
+    }
+}
 
 
 
--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const {
   checkDir,
 } = require("./app");
 // mdLinks(path, options)
-const mdLinks = (path, options) => {
-  
+
+const mdLinks = (path, options = {validate: false, stats: false }) => {
+
     return new Promise((resolve, reject) => {
-        if (options[0] === undefined && options[1] === undefined) {
+      resolve(5)
+        if (options) {
             console.log('tienes una option')
-            if (fileExists(path)) {
+           if (fileExists(path)) {
               if (!checkPath(path)) {
                 getAbsolutePath(path)
                   .then((absolutePath) => {
@@ -40,109 +42,17 @@ const mdLinks = (path, options) => {
                   })
                   .catch((error) => console.error(error));
               }
-            }
-    
-        } else {
-            if ((options[0] === '--validate' && options[1] === '--stats') || (options[0] === '--stats' && options[1] === '--validate')) {
-                console.log('Entered options : --validate and --stats');
-                // console.log(path)
-               /* if (fileExists(path)) {
-                  if (!checkPath(path)) {
-                    getAbsolutePath(path)
-                      .then((absolutePath) => {
-                        const files = checkDir(absolutePath);
-                        const renderFile = files.map((file) => extractLinks(file));
-                        const promesa = Promise.all(renderFile);
-                        //console.log("file", promesa)
-                        promesa.then((result) => {
-                          const planarArray = result.reduce(
-                            (acc, val) => acc.concat(val),
-                            []
-                          );
-                         // console.log("plana arr", planarArray);
-                         processLinks(planarArray)
-                         .then((result) => {
-                          resolve(result)
-                          }) 
-                         .catch((error)=> console.log("error",error));
-                         // console.log("validate",  validate)
-                          
-                        });
-                      })
-                      .catch((error) => console.error(error));
-                  }
-                }*/
-            } else if (options[0] === '--validate') {
-                console.log('Entered option: --validate');
-                // console.log(path)
-               /* if (fileExists(path)) {
-                  if (!checkPath(path)) {
-                    getAbsolutePath(path)
-                      .then((absolutePath) => {
-                        const files = checkDir(absolutePath);
-                        const renderFile = files.map((file) => extractLinks(file));
-                        const promesa = Promise.all(renderFile);
-                        //console.log("file", promesa)
-                        promesa.then((result) => {
-                          const planarArray = result.reduce(
-                            (acc, val) => acc.concat(val),
-                            []
-                          );
-                         // console.log("plana arr", planarArray);
-                         processLinks(planarArray)
-                         .then((result) => {
-                          resolve(result)
-                          }) 
-                         .catch((error)=> console.log("error",error));
-                         // console.log("validate",  validate)
-                          
-                        });
-                      })
-                      .catch((error) => console.error(error));
-                  }
-                }**/
-
-                
-            } else if (options[0] === '--stats') {
-                console.log('Entered options: --stats')
-                console.log(path);
-               /* if (fileExists(path)) {
-                  if (!checkPath(path)) {
-                    getAbsolutePath(path)
-                      .then((absolutePath) => {
-                        const files = checkDir(absolutePath);
-                        const renderFile = files.map((file) => extractLinks(file));
-                        const promesa = Promise.all(renderFile);
-                        //console.log("file", promesa)
-                        promesa.then((result) => {
-                          const planarArray = result.reduce(
-                            (acc, val) => acc.concat(val),
-                            []
-                          );
-                         // console.log("plana arr", planarArray);
-                         processLinks(planarArray)
-                         .then((result) => {
-                          resolve(result)
-                          }) 
-                         .catch((error)=> console.log("error",error));
-                         // console.log("validate",  validate)
-                          
-                        });
-                      })
-                      .catch((error) => console.error(error));
-                  } 
-                }*/
+            }  
+            //reject  
+                 
             } else {
                 console.log('Please enter a valid option');
                 //console.log(path)
             }
-        }
+        
     })
+
 }
 
 module.exports = { mdLinks }
 
-// const array3 = getAllMdDFiles('C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder');
-// console.log(array3);
-// const array4 = getAllMdDFiles('C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder/anotherFolder/randomStuff/theRaven.md');
-// console.log(array4);
